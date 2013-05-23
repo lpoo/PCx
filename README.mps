16564aa77b02235a38a4add8e9513fadc8a3da32
Raniere Silva <ra092767@ime.unicamp.br>
2013-05-23 10:10:36 -0300

    Improve bench.py to fix bugs.
    The script options have been improved and some check for erros
    have been introduced.
    
    Add the NETLIB-LP problems in the folder `mps` (compress using
    bzip2).
    
    The `configure` have been modified to uncompress all the tests.

---
+++ README.mps
@@ -0,0 +1,10 @@
+In the folder ``mps`` you will find some linear programming problem from some
+test library, in the mps format, that have been use to test PCx.
+
+NETLIB-LP
+---------
+
+The name of the problems files from NETLIB-LP start with ``netlib-``. You can
+find it in http://www.netlib.org/lp/data/.
+
+For more information see ``README.netlib-lp``.
--- bench.py
+++ bench.py
@@ -52,21 +52,66 @@ class PTInfo():
     def csv_header(self):
         return ("Name,Rows b.p.,Cols b.p.,Rows a.p.,Cols a.p.,Relative Infeas,"
                 "Relative Compl,Primal Objective,Max Add. Corr.,Iters,CPU Time"
-                " [secs]")
+                " [secs]\n")
 
     def csv(self):
-        return "{},{},{},{},{},{},{},{},{},{},{}".format(self.name, self.brows,
+        return "{},{},{},{},{},{},{},{},{},{},{}\n".format(self.name, self.brows,
                 self.bcols, self.arows, self.acols, self.rel_infeas,
                 self.rel_compl, self.prim_obj, self.max_corr, self.it, self.cpu)
 
-def bench(out_file, path, exclude):
+def bench(p, o, mf, n, s, i, x):
+    """
+    Run the benchmark.
+
+    :param p: Path of the files to be used.
+    :type p: str
+    :param o: Output file name.
+    :type o: str
+    :param mf: Maximum number of files to be used.
+    :type mf: int
+    :param n: Sort list of files to be used by name.
+    :type n: bool
+    :param s: Sort list of files to be used by size.
+    :type n: bool
+    :param i: List of files to be used for the benchmark.
+    :type i: list of str
+    :param x: List of files to be exclude from the benchmark.
+    :type i: list of str
+    """
     import subprocess
 
-    of = open(out_file, 'w')
+    # Check if path exist.
+    if not os.path.exists(p):
+        raise FileNotFoundError("can't find directory '{0}'".format(p))
+
+    of = open(o, 'w')
     of.write(PTInfo('').csv_header())
 
-    for f in os.listdir(path):
-        subprocess.call(["./PCx", f])
+    # Process list of files to be used in the benchmark.
+    if i:
+        fl = i
+    else:
+        fl = os.listdir(p)
+        if n:
+            fl.sort()
+        elif s:
+            fl.sort(key=lambda fname: os.path.getsize('{0}/{1}'.format(p,fname)))
+        if mf:
+            fl = fl[0:mf]
+        if x:
+            for r in x:
+                try:
+                    fl.remove(r)
+                except ValueError:
+                    pass
+
+    # Run PCx for every file in ``fl``.
+    for f in fl:
+        call_stdout = open("/dev/null", 'w')
+        print("Running PCx for {0}. It can take some minutes.".format(f))
+        subprocess.call(["./PCx", f], stdout=call_stdout)
+        print("End running PCx.")
+        call_stdout.close()
         with open(f.replace("mps", "log"), 'r') as log:
             info = PTInfo(f)
             step = 0  # step is responsible to select the regex to be used.
@@ -74,7 +119,6 @@ def bench(out_file, path, exclude):
                 if step == 0:
                     m = re.match('Iterations=(?P<it>[0-9]*), Termination Code=(?P<status>[0-9])', l)
                     if m:
-                        print(l)
                         info.add_status(m.group('status'))
                         info.add_it(m.group('it'))
                         step += 1
@@ -85,7 +129,6 @@ def bench(out_file, path, exclude):
                         info.add_bcols(m.group('cols'))
                         step += 1
                 elif step == 2:
-                    print(l)
                     m = re.match('.*After  Presolving:.* (?P<rows>[0-9]*) rows,.* (?P<cols>[0-9]*) columns', l)
                     if m:
                         info.add_arows(m.group('rows'))
@@ -112,31 +155,48 @@ def bench(out_file, path, exclude):
                         info.add_cpu(m.group('cpu'))
                         step += 1
             of.write(info.csv())
-        subprocess.call(["rm", f.replace(".mps", "*")])
+        subprocess.call(["rm", f.replace(".mps", ".log")])
     of.close()
 
-def check_spc_file(f2check):
-    """This function check if a given specification file can be used."""
-    f_correct = False
+def check_spc_file(f2check, p):
+    """
+    This function check if a given specification file can be used.
+
+    :param f2check: Name of the file to be checked.
+    :type f2check: str
+    :param p: Path to be used when check the file.
+    :type p: str
+    """
     with open(f2check, 'r') as f:
         for l in f:
             m = re.match('history.*(?P<toggle>(yes|no|))', l)
             if m:
                 if m.group('toggle') in ['yes', '']:
-                    f_correct = True
+                    history = True
                 elif m.group('toggle') == 'no':
-                    f_correct == False
-    return f_correct
+                    history == False
+            m = re.match('inputdirectory (?P<dir>.*)', l)
+            if m:
+                if m.group('dir') == p:
+                    path = True
+                else:
+                    path = False
+    return history and path
 
-def check_spc():
-    """This function check if the specifications file can be used."""
+def check_spc(path):
+    """
+    This function check if the specifications file can be used.
+
+    :param p: Path to be used when check the file.
+    :type p: str
+    """
     spc_correct = False
     if os.path.isfile('spc'):
-        spc_correct = check_spc_file('spc')
+        spc_correct = check_spc_file('spc', path)
     elif os.path.isfile('specs'):
-        spc_correct = check_spc_file('sepcs')
+        spc_correct = check_spc_file('sepcs', path)
     elif os.path.isfile('PCx.specs'):
-        spc_correct = check_spc_file('PCx.specs')
+        spc_correct = check_spc_file('PCx.specs', path)
     return spc_correct
 
 if __name__ == "__main__":
@@ -154,22 +214,28 @@ if __name__ == "__main__":
     # Parse of flags.
     parser = argparse.ArgumentParser(description='Benchmark for PCx.',
             formatter_class=RawTextHelpFormatter)
-    parser.add_argument('--max', type=int, default=[],
-            help="List of files to be used for the benchmark.\n")
-    parser.add_argument('-o', type=str, default='bench.out',
-            help='Output file.')
-    parser.add_argument('--only', nargs='+', type=str, default=[],
-            help="List of files to be used for the benchmark.\n")
-    parser.add_argument('-p', type=str, default='mps',
-            help='Path of the files to test.')
-    parser.add_argument('-s', type=str, default='name',
-            help='Path of the files to test.')
-    parser.add_argument('-x', nargs='+', type=str, default=[],
-            help="List of files to be exclude from the benchmark.\n")
+    parser.add_argument('-p', '--path', type=str, default='./mps',
+            help='Path of the files to be used.')
+    parser.add_argument('-o', '--output', type=str, default='bench.out',
+            help='Output file name.')
+    parser.add_argument('--max', type=int, default=None,
+            help="Maximum number of files to be used.")
+    bench_sort = parser.add_mutually_exclusive_group()
+    bench_sort.add_argument('-n', '--name', action='store_true',
+            help='Sort list of files to be used by name.')
+    bench_sort.add_argument('-s', '--size', action='store_true',
+            help='Sort list of files to be used by size.')
+    bench_file = parser.add_mutually_exclusive_group()
+    bench_file.add_argument('-i', '--input', nargs='+', type=str, default=[],
+            help="List of files to be used for the benchmark.")
+    bench_file.add_argument('-x', '--exclude', nargs='+', type=str, default=[],
+            help="List of files to be exclude from the benchmark.")
 
     args = parser.parse_args()
 
-    if check_spc():
-        bench(args.o, args.p, args.x)
+    if check_spc(args.path):
+        pass
+        bench(args.path, args.output, args.max, args.name, args.size,
+                args.input, args.exclude)
     else:
         print("The specification file must contain 'history yes'.\n")
